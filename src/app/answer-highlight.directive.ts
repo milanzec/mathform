dfae851601e7b1c7dd0b73b5611f394b3cb6ec3e
unknown <milanzec53@gmail.com>
2023-09-19 01:25:34 +0200

    map operator

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { map, filter } from 'rxjs/operators'
 
 
 @Directive({
@@ -13,7 +14,11 @@ export class AnswerHighlightDirective {
 
   ngOnInit() {
 
-    console.log(this.controlName.control?.parent)
+    this.controlName.control?.parent?.valueChanges.pipe(
+      map(({ a, b, answer }) => Math.abs((a + b - answer) / a + b))
+    ).subscribe((value) => {
+        console.log(value)
+      })
   }
 
 }
